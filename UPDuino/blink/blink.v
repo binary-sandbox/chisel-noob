9b3b8ba10b91e0156bfd49e1fb8c66cbd2686daf
Kevin Patino <kevindpatino@gmail.com>
2019-01-08 23:04:54 -0600

    Scala blinker example with Upduino Lattice FPGA

--- UPDuino/blink/blink.v
+++ UPDuino/blink/blink.v
@@ -14,15 +14,15 @@ module blink
 
   SB_HFOSC  u_SB_HFOSC(.CLKHFPU(1), .CLKHFEN(1), .CLKHF(int_osc));
 
-  always @(posedge int_osc) begin
-    frequency_counter_i <= frequency_counter_i + 1'b1;
-  end
+  wire led[2:0];
 
+  blink_core blink0(.clock(int_osc),.reset(rstn),.io_led1(led[0]),.io_led2(led[1]),.io_led3(led[2]));
+  
   SB_RGBA_DRV RGB_DRIVER ( 
     .RGBLEDEN (1'b1),
-    .RGB0PWM  (frequency_counter_i[24]&frequency_counter_i[23]),
-    .RGB1PWM  (frequency_counter_i[24]&~frequency_counter_i[23]),
-    .RGB2PWM  (~frequency_counter_i[24]&frequency_counter_i[23]),
+    .RGB0PWM  (led[0]),
+    .RGB1PWM  (~led[1]),
+    .RGB2PWM  (led[0]|led[1]),
     .CURREN   (1'b1), 
     .RGB0     (REDn),		//Actual Hardware connection
     .RGB1     (GRNn),
